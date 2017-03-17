3d5d7a985e0e3dc37f0bd7fced4680b002912496
Allef Sousa <allefsousa_1@hotmail.com>
2017-03-17 19:56:13 -0300

    GrafosMatrizAdjacencia

--- src/projetoteoriadosgrafos/ProjetoTeoriaDosGrafos.java
+++ src/projetoteoriadosgrafos/ProjetoTeoriaDosGrafos.java
@@ -6,6 +6,7 @@
 package projetoteoriadosgrafos;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,26 +16,72 @@ public class ProjetoTeoriaDosGrafos {
 
     /**
      * @param args the command line arguments
+     *
      */
     public static void main(String[] args) {
-        int op =0;
+        
+        int op = 0, ver = 0, linha = 0, coluna = 0;
+        int loop = 0;
         Scanner scaner = new Scanner(System.in);
+        
         System.out.println("Digite 1 para Grafo ou 2 Para Digrafo :");
         op = scaner.nextInt();
-        
-        if(op == 1){
-            System.out.println("Grafo ... ");
-            System.out.println("");
-        }else if (op == 2 ){
-            System.out.println("Digrafo ... ");
+        System.out.println("Informe a Quantidade de Vertices");
+        ver = scaner.nextInt();
+        System.out.println("Informe a Quantidade de Arestas");
+        loop = scaner.nextInt();
+        int mat[][] = new int[ver][ver];
 
+        if (op == 1) {
+            System.out.println("Grafo ... ");
+
+            for (int i = 0; i < loop; i++) {
+
+                System.out.println("Informe o Vertice a ser ligado ");
+                linha = scaner.nextInt();
+                System.out.println("Informe o Vertice que recebe a ligação ");
+                coluna = scaner.nextInt();
+                if (linha == coluna) { // recursão
+                    mat[linha - 1][coluna - 1] = 1;
+                } else {
+                    mat[linha - 1][coluna - 1] = 1;
+                    mat[coluna - 1][linha - 1] = 1;
+                }
+
+            }
+            mostrarMatri(ver, mat);
+
+        } else if (op == 2) {
+            System.out.print("Digrafo ... ");
+            for (int i = 0; i < loop; i++) {
+
+                System.out.println("Informe o Vertice a ser ligado ");
+                linha = scaner.nextInt();
+                System.out.println("Informe o Vertice que recebe a ligação ");
+                coluna = scaner.nextInt();
+                if (linha == coluna) { // recursão
+                    mat[linha - 1][coluna - 1] = 1;
+                } else {
+                    mat[linha - 1][coluna - 1] = 1;
+                   // mat[coluna - 1][linha - 1] = 1;
+                }
+
+            }
+            mostrarMatri(ver, mat);
             
+
         }
-        
-        
-        
-        
-      
+
     }
-    
+
+    public static void mostrarMatri(int v, int ma[][]) {
+        System.out.println("## MATRIZ ##");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                System.out.print(ma[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+    }
+
 }
